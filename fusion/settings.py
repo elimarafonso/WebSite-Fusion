677d58937de88ff38763233f08c89f38d2cfc892
Elimar Afonso <elimar_afonso@hotmail.com>
2022-11-29 10:54:25 -0300

    mudando interface Admin

--- fusion/settings.py
+++ fusion/settings.py
@@ -34,6 +34,12 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    # General use templates & template tags (should appear first)
+    'adminlte3',
+    # Optional: Django admin theme (must be before django.contrib.admin)
+    'adminlte3_theme',
+    'core',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'core',
+
 ]
 
 MIDDLEWARE = [
